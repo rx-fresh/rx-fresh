53d95cba3c7dcab468a094692dd522aacca5c3c0
leaderofcults-pixel <leaderofcults@gmail.com>
2025-09-17 20:21:24 +0000

    gg

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,15 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      server: {
+        proxy: {
+          '/api': {
+            target: 'https://api.rxprescribers.com',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          }
+        }
       }
     };
 });
